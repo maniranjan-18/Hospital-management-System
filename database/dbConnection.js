7000b6a4942c3eaad944bb39c2bc278037ba5847
maniranjan-18 <maniranjanpathak449@gmail.com>
2024-06-26 12:22:28 +0530

    first commit

---
+++ database/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
+    })
+    .then(() => {
+      console.log("connected to database");
+    })
+    .catch((err) => {
+      console.log(`error aa gya yar ${err}`);
+    });
+};
---
+++ server.js
@@ -0,0 +1,11 @@
+import app from "./app.js";
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLODINARY_NAME,
+  api_key: process.env.CLODINARY_API_KEY,
+  api_secret: process.env.CLODINARY_API_SECRET,
+});
+app.listen(process.env.PORT, () => {
+  console.log(`server listenig on port ${process.env.PORT}`);
+});
